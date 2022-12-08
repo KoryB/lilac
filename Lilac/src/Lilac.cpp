8549b6a646187fc77c315a5a653036b9e4d63db5
KoryB <byrnekory@gmail.com>
2022-12-08 16:48:50 -0500

    Updated version to 0.0.1

--- Lilac/src/Lilac.cpp
+++ Lilac/src/Lilac.cpp
@@ -2,6 +2,7 @@
 // Headers
 ////////////////////////////////////////////////////////////
 #include <SFML3D/Graphics.hpp>
+#include <SFML3D/Window.hpp>
 #include <cmath>
 #include <fstream>
 #include <sstream>
@@ -45,18 +46,19 @@ int main()
     window.setView(camera);
 
     // Create a cube to demonstrate transform and lighting effects
-    sf3d::Cuboid cube(sf3d::Vector3f(5, 5, 5));
-    cube.setColor(sf3d::Color::Red);
-    cube.setPosition(20, 0, -50);
-
-    // Create a sphere to demonstrate transform and lighting effects
-    sf3d::SphericalPolyhedron sphere(5);
-    sphere.setColor(sf3d::Color::Cyan);
-    sphere.setPosition(-20, 0, -50);
-
-    // Create a sphere to mark our light position
-    sf3d::SphericalPolyhedron lightSphere(2, 1);
-    lightSphere.setColor(sf3d::Color::Yellow);
+    std::vector<sf3d::Cuboid> cubes{};
+    for (int x = -1; x <= 1; x++)
+    {
+        for (int y = -1; y <= 1; y++)
+        {
+            for (int z = -1; z <= 1; z++)
+            {
+                cubes.emplace_back(sf3d::Vector3f(1, 1, 1));
+                cubes.back().setColor(sf3d::Color::Red);
+                cubes.back().setPosition(2 * x, 2 * y, 2 * z);
+            }
+        }
+    }
 
     // Create a clock for measuring the time elapsed
     sf3d::Clock clock;
@@ -114,7 +116,6 @@ int main()
             }
         }
 
-        // Keep the mouse cursor within the window
         sf3d::Mouse::setPosition(sf3d::Vector2i(window.getSize()) / 2, window);
 
         // Update our virtual camera orientation/position based on user input
@@ -152,13 +153,23 @@ int main()
         if (sf3d::Keyboard::isKeyPressed(sf3d::Keyboard::D))
             camera.move(rightVector * 50.f * delta);
 
-        // Space bar pressed : move upwards
-        if (sf3d::Keyboard::isKeyPressed(sf3d::Keyboard::Space))
-            camera.move(0, 50 * delta, 0);
+        if (sf3d::Keyboard::isKeyPressed(sf3d::Keyboard::LAlt))
+        {
+            sf3d::Texture t;
+            t.create(window.getSize().x, window.getSize().y);
+            t.update(window);
+            t.copyToImage().saveToFile("screenshot.png");
+            return EXIT_SUCCESS;
+        }
+            
 
-        // Shift key pressed : move downwards
-        if (sf3d::Keyboard::isKeyPressed(sf3d::Keyboard::LShift))
-            camera.move(0, -50 * delta, 0);
+        //// Space bar pressed : move upwards
+        //if (sf3d::Keyboard::isKeyPressed(sf3d::Keyboard::Space))
+        //    camera.move(0, 50 * delta, 0);
+
+        //// Shift key pressed : move downwards
+        //if (sf3d::Keyboard::isKeyPressed(sf3d::Keyboard::LShift))
+        //    camera.move(0, -50 * delta, 0);
 
         // Inform the window to update its view with the new camera data
         window.setView(camera);
@@ -166,30 +177,23 @@ int main()
         // Clear the window
         window.clear();
 
-        cube.rotate(50 * delta, sf3d::Vector3f(0.5f, 0.9f, 0.2f));
-        lightSphere.rotate(180 * delta, sf3d::Vector3f(0.7f, 0.2f, 0.4f));
-
         // Make the light source orbit around the scene
         sf3d::Vector3f newOrbitPosition(50 * std::cos(elapsedSeconds / 6),
             30 * std::cos(elapsedSeconds / 6),
             20 * std::sin(elapsedSeconds / 6));
         light.setPosition(sf3d::Vector3f(0, 0, -50) + newOrbitPosition);
 
-        // Set the sphere to the same position as the light source
-        lightSphere.setPosition(light.getPosition());
-
         // Disable lighting for the text and the light sphere
         sf3d::Light::disableLighting();
-
-        // Draw the sphere representing the light position
-        window.draw(lightSphere);
 
         // Enable lighting again
         sf3d::Light::enableLighting();
 
         // Draw the cube, sphere and billboard
-        window.draw(cube);
-        window.draw(sphere);
+        for (auto& cube : cubes)
+        {
+            window.draw(cube);
+        }
 
         // Disable lighting and reset to 2D view to draw information
         sf3d::Light::disableLighting();
